e8d99283b043d763e14ff68e10df57fa57b6c317
rupam2001 <rjdasgalexy@gmail.com>
2021-06-17 20:28:04 +0530

    footer added

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div>
-
+        <div className="footer-main">
+            <p className="jecglug">part of <a href="http://jecglug.in" target="_blank" rel="noopener noreferrer">www.jecglug.in</a></p>
         </div>
     )
 }
